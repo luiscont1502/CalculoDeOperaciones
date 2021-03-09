72f15b255a10ce521d530bbff4ec406819fdc4ba
Luis <luiscont@gmail.com>
2021-03-09 11:15:53 -0500

    Operaciones por seleccion

--- src/figura/Figura.java
+++ src/figura/Figura.java
@@ -10,14 +10,61 @@ public class Figura {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int base,altura,n=0;
-           System.out.println("\t\tTriangulo");
+          
+        do{
+        System.out.println("\t\t\tMenu");
+        System.out.println("Seleccione la operacion a realizar");
+        System.out.println("\t1.- Cuadrado");
+        System.out.println("\t2.- Rectangulo");
+        System.out.println("\t3.- Triangulo");
+        System.out.println("\t0.- Salir");
+        System.out.println("Escoja la opcion");
+        n=sc.nextInt();
+        switch(n){
+           
+            case 0:
+                System.out.println("Gracias por visitarnos");
+                break;
+            case 1:
+                System.out.println("\t\tCuadrado");
+                System.out.println("Ingrese el lado del cuadrado");
+                int a=sc.nextInt();
+                Cuadrado(a);
+                break;
+            case 2:
+                System.out.println("\t\tRectangulo");
                 System.out.println("Ingrese la base");
                 base=sc.nextInt();
                 System.out.println("Ingrese la altura");
                 altura=sc.nextInt();
-                float res=(base*altura)/2;
-          System.out.println("El area es :" +res);
-          
+                Rectangulo(base,altura);
+                break;
+            case 3:
+                  System.out.println("\t\tTriangulo");
+                System.out.println("Ingrese la base");
+                base=sc.nextInt();
+                System.out.println("Ingrese la altura");
+                altura=sc.nextInt();
+                Triangulo(base, altura);
+                break;
+            default:
+                System.out.println("No se encuentra en la lista de operaciones");
+                break;
+        }
+            
+        }while(n!=0);
     }
+      public static void Cuadrado(int a){
+        int res =a*a;
+          System.out.println("El area es : "+res);
+        }
+      public static void Rectangulo(int a, int b){
+      int res=a*b;
+          System.out.println("El Area es : "+res);
+      }
+      public static void Triangulo(int a, int b){
+          float res=(a*b)/2;
+          System.out.println("El area es :" +res);
+      }
     
 }
